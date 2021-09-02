7af6b8d2874e168191e71e4c9f62f21a78b6353c
abinshaabi <imabinsha@gmail.com>
2021-09-02 16:12:04 +0530

    add food card

---
+++ client/src/Components/Restaurant/Order-Online/FoodItem.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { BiPlus } from 'react-icons/bi'
+import ReactStars from 'react-rating-stars-component'
+
+const FoodItem = (props) => {
+    return (
+        <>
+            <div className="my-5 flex items-start gap-2 md:gap-3">
+                <div className="w-3/12 h-24 md:h-28 lg:h-36">
+                    <img src={props.image}
+                     alt="food"
+                     className="w-full h-full  rounded-lg "
+                     />
+                </div>
+                <div className="w-9/12 md:w-7/12 h-full flex flex-col ">
+                    <div className="flex items-center justify-between">
+                        <h3 className="md:text-xl font-semibold">
+                            {props.title}
+                        </h3>
+                        <button className="md:hidden flex items-center gap-2 text-zomato-400 bg-zomato-50 border border-zomato-400 px-2 py-1 md:px-4 md:py-2 rounded-lg"><BiPlus /> Add</button>
+
+                    </div>
+                    <ReactStars count={5}  value={props.rating} edit={false} />
+                    <h5 className="md:mt-3 text-lg">₹{props.price}</h5>
+                    <p className="truncate">
+                        {props.description}
+                    </p>
+                </div>
+                <div className="hidden md:block w-3/12">
+                    <button className="flex items-center gap-2 text-zomato-400 bg-zomato-50 border border-zomato-400 px-4 py-1 lg:py-2 rounded-lg"><BiPlus /> Add</button>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default FoodItem
--- client/src/Pages/Restaurant/OrderOnline.jsx
+++ client/src/Pages/Restaurant/OrderOnline.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { AiOutlineCompass } from 'react-icons/ai'
+import { BiTimeFive } from 'react-icons/bi'
+
+//components
 import FloatMenuButton from '../../Components/Restaurant/Order-Online/FloatMenuButton'
+import FoodItem from '../../Components/Restaurant/Order-Online/FoodItem'
 import MenuListContainer from '../../Components/Restaurant/Order-Online/MenuListContainer'
 
 const OrderOnline = () => {
     return (
        <>
-            <div className="w-full">
-                <aside className="hidden md:flex flex-col gap-5 border-r border-gray-200 overflow-y-scroll h-screen w-1/4">
+            <div className="w-full flex ">
+                <aside className="hidden md:flex flex-col gap-5 border-r border-gray-200   h-screen w-1/4">
                     <MenuListContainer />
                     <MenuListContainer />
                     <MenuListContainer />
@@ -15,7 +20,22 @@ const OrderOnline = () => {
                     
                     <MenuListContainer />
                 </aside>
-                <div className="w-full md:w-3/4"></div>
+                <div className="w-full md:w-3/4">
+                    <h2 className="text-xl md:text-2xl lg:text-2xl font-normal mb-2">Order Online</h2>
+                    <h4 className="flex items-center gap-2 font-light text-gray-500">
+                        <AiOutlineCompass /> Live Track Your Order | <BiTimeFive /> 45 min
+                    </h4>
+
+                    <section>
+                        <FoodItem
+                         image="https://b.zmtcdn.com/data/dish_photos/839/62575c08ce26635e3f48b3642805d839.jpg?output-format=webp&fit=around|130:130&crop=130:130;*,*" 
+                         title="Andhra Mutton Biryani Family Pack"
+                         rating={3}
+                         price="869"
+                         description="In fact, inserting any fantasy text or a famous text, be it a poem, a speech, a literary passage, a song's text, etc., our text generator will provide the random extraction of terms and steps to compose your own exclusive Lorem Ipsum."
+                        />
+                    </section>
+                </div>
             </div>
             <FloatMenuButton />
        </>
